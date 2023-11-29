d0c6eb85afcdc3d3bfb6b43de45ac1ee10d38cd2
Khaled Vela <80561953+khaledVela@users.noreply.github.com>
2023-11-29 00:56:23 -0400

    Update funciones.py

--- apiejemplo/apis/funciones.py
+++ apiejemplo/apis/funciones.py
@@ -1,8 +1,8 @@
 def DefinEstrella(request):
     tiempo = request.data['tiempo']
     if tiempo < 60:
-        return 3
-    if tiempo < 120:
-        return 2
-    if tiempo < 180:
         return 1
+    elif tiempo > 60 and tiempo < 90:
+        return 2
+    elif tiempo > 90 and tiempo < 180:
+        return 3
