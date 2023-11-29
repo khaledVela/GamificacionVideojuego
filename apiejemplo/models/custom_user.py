0725f3049d49cfa8dcb8863c4ca6c909a0df043f
Khaled Vela <80561953+khaledVela@users.noreply.github.com>
2023-11-28 22:41:47 -0400

    Add files via upload

--- apiejemplo/models/custom_user.py
+++ apiejemplo/models/custom_user.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    tipo = models.IntegerField()
-    last_level = models.IntegerField()
-    vidaslvl = models.IntegerField()
+    tipo = models.IntegerField(null=True, blank=True)
+    last_level = models.IntegerField(null=True, blank=True)
+    vidaslvl = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.username
